a19c3b7a08efebbace31cc5ffe17a529867df2ff
OffTheWaller <1031118178@qq.com>
2018-10-10 20:05:26 +0800

    footer

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,10 +2,5 @@
  * @author: js
  */
 'use strict';
-var _myUtil = require('util/myutil.js')
-var html = '<h1>{{msg}}</h1>';
-var data = {
-    msg: 'hello'
-};
-console.log(_myUtil.renderHtml(html, data))
+require('page/common/nav/index.js')
 
