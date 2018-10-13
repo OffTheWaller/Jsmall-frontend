2944dfea449d735b5cbde5d0b830b272463a7452
OffTheWaller <1031118178@qq.com>
2018-10-13 10:45:39 +0800

    完成header组件

--- src/page/common/header/index.js
+++ src/page/common/header/index.js
@@ -1,1 +1,49 @@
+/**
+ * @author: js
+ */
+'use strict'
 require('./index.css')
+
+var _myUtil = require('util/myutil.js')
+
+
+//通用页面头部
+var header = {
+    init: function () {
+        this.bindEvent();
+    },
+    onLoad: function () {
+        // 搜索信息的回填
+        var keyword = _myUtil.getUrlParam('keyword');
+        if (keyword) {
+            $('#search-input').val(keyword)
+        };
+    },
+    bindEvent: function () {
+        //提交
+        var _this = this;
+        //点击搜索按钮提交
+        $('#search-btn').click(function () {
+            _this.searchSubmit();
+        });
+        //输入回车后提交
+        $('#search-input').keyup(function (e) {
+            // 13是回车键的keyCode
+            if (e.keyCode == 13) {
+                _this.searchSubmit();
+            }
+        })
+    },
+    //搜索提交
+    searchSubmit: function () {
+        var keyword = $.trim($('#search-input').val());
+        if (keyword) {
+            window.location.href = './list.html?keyword=' + keyword;
+        } else {
+            _myUtil.goHome();
+        }
+    }
+
+}
+
+header.init();
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -3,4 +3,5 @@
  */
 'use strict';
 require('page/common/nav/index.js')
+require('page/common/header/index.js')
 
