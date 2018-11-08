136c8d01d9ef69a00b9270f572260b309aa77638
OffTheWaller <1031118178@qq.com>
2018-11-08 22:01:39 +0800

    首页banner区域

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,11 +2,28 @@
  * @author: js
  */
 'use strict';
+require('./index.css')
 require('page/common/nav/index.js')
-var navSide = require('page/common/nav-side/index.js')
 require('page/common/header/index.js')
+require('util/slider/index.js')
 
-navSide.init({
-    name: ''
-});
+var templateBanner = require('./banner.string')
+var _myUtil = require('util/myutil.js')
+
+$(function () {
+    //渲染banner的html
+    var bannerHtml = _myUtil.renderHtml(templateBanner);
+    $('.banner-con').html(bannerHtml);
+    //初始化slider
+    var $slider = $('.banner').unslider({
+        dots: true
+    });
+    // 前一张和后一张的事件绑定
+    $('.banner-con .banner-arrow').click(function () {
+        var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+        $slider.data('unslider')[forward]();
+    })
+})
+
+
 
